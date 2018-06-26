ae30bbce6f36f84d6d36e5e05b420730396abf18
lemon <422896102@qq.com>
2018-06-26 15:49:55 +0800

    init qunar

---
+++ src/main.js
@@ -0,0 +1,26 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import fastclick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
+
+
+Vue.config.productionTip = false
+//fastclick解决某些设备 click事件300毫秒延迟
+fastclick.attach(document.body)
+//轮播图插件
+Vue.use(VueAwesomeSwiper)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
