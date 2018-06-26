3018631ce4c0785c98b90467a5602aef93389302
lemon <422896102@qq.com>
2018-06-26 17:49:24 +0800

    首页渲染

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,8 +1,11 @@
 <template>
     <div>
-        <home-header></home-header>
-        <home-swiper></home-swiper>
-        <home-icons></home-icons>
+        <!--父组件向子组件传值利用属性v-bind:city　简写 :city -->
+        <home-header :city="city"></home-header>
+        <home-swiper :list="swiperList"></home-swiper>
+        <home-icons  :list="iconList"></home-icons>
+        <home-recommend :list="recommendList"></home-recommend>
+        <home-weekend :list="weekendList"></home-weekend>
     </div>
 </template>
 
@@ -10,13 +13,53 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
+import axios from 'axios'
+
+//父组件
 export default {
     name: 'Home',
+    //注册子组件
     components: {
         HomeHeader,
         HomeSwiper,
-        HomeIcons
-    }    
+        HomeIcons,
+        HomeRecommend,
+        HomeWeekend
+    },
+    data (){
+        return {
+            city: '',
+            swiperList: [],
+            iconList:  [],
+            recommendList: [],
+            weekendList: []
+        }
+    },
+    methods: {
+        getHomeInfo (){
+            //通过config index.js 里面proxyTable转发
+            axios.get('/api/index.json')
+                .then(this.getHomeInfoSucc)
+        },
+        getHomeInfoSucc (res){
+            console.log(res)
+            res = res.data
+            if (res.ret && res.data){
+                const data = res.data
+                this.city = data.city
+                this.swiperList = data.swiperList
+                this.iconList = data.iconList
+                this.recommendList = data.recommendList
+                this.weekendList = data.weekendList
+                //console.log(this.swiperList)
+            }
+        }
+    },
+    mounted() {
+        this.getHomeInfo()     
+    },    
 }
 </script>
 
--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="wrapper">
-    <swiper :options="swiperOption">
+    <swiper :options="swiperOption" v-if="showSwiper">
       <!-- slides -->
-      <swiper-slide v-for="item of swiperList" :key="item.id" >
-        <img class="swiper-img" :src="item.imUrl"/>
+      <swiper-slide v-for="item of list" :key="item.id" >
+        <img class="swiper-img" :src="item.imgUrl"/>
       </swiper-slide>
       <!-- Optional controls -->
       <div class="swiper-pagination"  slot="pagination"></div>
@@ -15,19 +15,20 @@
 //子组件
 export default {
     name: 'HomeSwiper',
+    props: {
+      list: Array
+    },
     data () {
       return {
         swiperOption : {
           pagination: '.swiper-pagination',
           loop: true
-        },
-        swiperList: [{
-          id: '0001',
-          imUrl: 'https://img1.qunarzz.com/piao/fusion/1802/e3/62ce7362ca051d02.jpg_640x200_6db551b7.jpg'
-        },{
-          id: '0002',
-          imUrl: 'https://img1.qunarzz.com/piao/fusion/1801/93/ce59d182aca07102.jpg_640x200_ba03d44c.jpg'
-        }]
+        }
+      }
+    },
+    computed: {
+      showSwiper (){
+        return this.list.length
       }
     } 
 }
