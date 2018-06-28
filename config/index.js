b25a3fa8dce1302c8d95aec3051ebc71226dd51c
lemon <422896102@qq.com>
2018-06-28 09:12:15 +0800

    *

--- config/index.js
+++ config/index.js
@@ -6,13 +6,12 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
       '/api' : {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://192.168.66.146:8080',
         pathRewrite: {
           '^/api': '/static/data'
         }
@@ -20,7 +19,7 @@ module.exports = {
     },
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '192.168.66.146', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
